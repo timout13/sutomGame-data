c97288c8d2cbd92ccf51c4d955132957e0c6c699
Timothe.L <timothe463@icloud.com>
2022-02-08 22:18:03 +0100

    Discovery--TailwindCss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+      './*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+      require('@tailwindcss/typography'),
+      require('@tailwindcss/forms')
+  ],
+}
